d94057f944128a17fb5177fbb97918947e4c582d
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-10-30 15:58:51 -0400

    Page principale-reste juste la logique des boutons a faire

--- src/main/java/ca/qc/bdeb/inf203/tp2/Main.java
+++ src/main/java/ca/qc/bdeb/inf203/tp2/Main.java
@@ -1,6 +1,13 @@
 package ca.qc.bdeb.inf203.tp2;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,6 +17,27 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+
+        PagePrincipale pagePrincipale=new PagePrincipale();
+
+        Scene scene=new Scene(pagePrincipale.PagePrincipale(),900,520);
+
+        //set la root de la page jeu
+        pagePrincipale.getBoutonInfo().setOnAction(event -> {
+        });
+
+
+
+
+        stage.setScene(scene);
+        stage.setTitle("Charlotte la Barbotte");
+        Image iconCharlotteLaBarbotte=new Image("charlotte.png");
+        stage.getIcons().add(iconCharlotteLaBarbotte);
+
+
+
+
+
         stage.show();
     }
 }
---
+++ src/main/java/ca/qc/bdeb/inf203/tp2/PagePrincipale.java
@@ -0,0 +1,42 @@
+package ca.qc.bdeb.inf203.tp2;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class PagePrincipale {
+
+    private final Button boutonJouer=new Button("Jouer!");
+    private final Button boutonInfo=new Button("Infos");
+
+    VBox PagePrincipale(){
+        VBox pagePrincipale=new VBox();
+        pagePrincipale.setAlignment(Pos.TOP_CENTER);
+        pagePrincipale.setSpacing(10);
+        HBox boutonsPagePrincipale=new HBox();
+        boutonsPagePrincipale.setAlignment(Pos.CENTER);
+        boutonsPagePrincipale.setSpacing(10);
+        boutonsPagePrincipale.getChildren().addAll(boutonJouer,boutonInfo);
+        ImageView logo=new ImageView("logo.png");
+        logo.setFitHeight(450);
+        logo.setFitWidth(450);
+        pagePrincipale.getChildren().addAll(logo,boutonsPagePrincipale);
+        pagePrincipale.setBackground(Background.fill(Color.valueOf("#2A7FFF")));
+        return pagePrincipale;
+    }
+
+    public Button getBoutonJouer() {
+        return boutonJouer;
+    }
+
+    public Button getBoutonInfo() {
+        return boutonInfo;
+    }
+}
