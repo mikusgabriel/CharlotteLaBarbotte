6b46bd12436238348a26ba6249ae79fb48ec9076
YogurtMold69 <cyberqraft@gmail.com>
2023-11-01 20:30:15 -0400

    edit page infos

--- src/main/java/ca/qc/bdeb/inf203/tp2/PageInfos.java
+++ src/main/java/ca/qc/bdeb/inf203/tp2/PageInfos.java
@@ -13,29 +13,22 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class PageInfos extends Parent {
-    private final Text titreDeveloppeurs = new Text();
-    private final Text informations = new Text();
-    private final Button buttonRetour = new Button();
-    private final Background arrierePlan = Background.fill(Color.valueOf("#2A7FFF"));
+    private final VBox pageInfos = new VBox();
 
     PageInfos(){
         Text titreJeu = new Text("Charlotte la Barbotte"),
                 par = new Text("par"),
                 et = new Text("et"),
                 dev1 = new Text("Gabriel Mikus"),
-                dev2 = new Text("Samuel Leuchtmann");
-
-
+                dev2 = new Text("Samuel Leuchtmann"),
+                informations = new Text("Travail remis à Nicolas Hurtubise et Georges Côté. Graphismes adaptés de https://games-icons.net/ et de https://openclipart.org/. Développé dans le cadre du cours 420-203-RE - Développement de programmes dans un environnement graphique, au Collège de Bois-de-Boulogne.");
         titreJeu.setFont(Font.font(30));
+        par.setFont(Font.font(15));
+        et.setFont(Font.font(15));
+        dev1.setFont(Font.font(20));
+        dev2.setFont(Font.font(20));
 
-        var imageCharlotte = new ImageView();
-        imageCharlotte.setImage(new Image("poisson1.png"));
-
-
-        var devBox1 = new HBox();
-        devBox1.getChildren().add(new Text(""))
-
-
+        var imageCharlotte = new ImageView(new Image("poisson1.png"));
 
     }
 
