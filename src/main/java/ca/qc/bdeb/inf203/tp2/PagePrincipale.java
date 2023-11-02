1ea47b16f819b9f2ffcc809dc497e8378bc7dbf3
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-11-01 20:22:50 -0400

    PagePrincipale plus clean

--- src/main/java/ca/qc/bdeb/inf203/tp2/PagePrincipale.java
+++ src/main/java/ca/qc/bdeb/inf203/tp2/PagePrincipale.java
@@ -16,27 +16,41 @@ public class PagePrincipale extends Parent {
     private final Button boutonJouer=new Button("Jouer!");
     private final Button boutonInfo=new Button("Infos");
 
-    VBox PagePrincipale(){
-        VBox pagePrincipale=new VBox();
+    private final Background arrierePlan = Background.fill(Color.valueOf("#2A7FFF"));
+
+    private VBox pagePrincipale=new VBox();
+
+    PagePrincipale(){
+        pagePrincipale=creationPagePrincipale();
+    }
+    private VBox creationPagePrincipale(){
+        var pagePrincipale=new VBox();
         pagePrincipale.setAlignment(Pos.TOP_CENTER);
         pagePrincipale.setSpacing(10);
-        HBox boutonsPagePrincipale=new HBox();
+
+        var boutonsPagePrincipale=new HBox();
         boutonsPagePrincipale.setAlignment(Pos.CENTER);
         boutonsPagePrincipale.setSpacing(10);
         boutonsPagePrincipale.getChildren().addAll(boutonJouer,boutonInfo);
-        ImageView logo=new ImageView("logo.png");
+
+        var logo=new ImageView("logo.png");
         logo.setFitHeight(450);
         logo.setFitWidth(450);
+
         pagePrincipale.getChildren().addAll(logo,boutonsPagePrincipale);
-        pagePrincipale.setBackground(Background.fill(Color.valueOf("#2A7FFF")));
+        pagePrincipale.setBackground(arrierePlan);
+
         return pagePrincipale;
     }
-
     public Button getBoutonJouer() {
         return boutonJouer;
     }
 
     public Button getBoutonInfo() {
         return boutonInfo;
+    }
+
+    public VBox getPagePrincipale() {
+        return pagePrincipale;
     }
 }
